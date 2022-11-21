2635c4b808a48d6c33137972666f460ea97f332d
powcoder <powcoder@163.com>
2022-11-21 16:21:15 +0800

    COMP251 HW3 F2019

---
+++ FordFulkerson.java
@@ -0,0 +1,82 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+import java.io.*;
+import java.util.*;
+
+
+
+
+public class FordFulkerson {
+
+	
+	public static ArrayList<Integer> pathDFS(Integer source, Integer destination, WGraph graph){
+		ArrayList<Integer> Stack = new ArrayList<Integer>();
+		/* YOUR CODE GOES HERE
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		*/
+		return Stack;
+	}
+	
+	
+	
+	public static void fordfulkerson(Integer source, Integer destination, WGraph graph, String filePath){
+		String answer="";
+		String myMcGillID = "26000000"; //Please initialize this variable with your McGill ID
+		int maxFlow = 0;
+		
+				/* YOUR CODE GOES HERE
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		*/
+		
+		
+		answer += maxFlow + "\n" + graph.toString();	
+		writeAnswer(filePath+myMcGillID+".txt",answer);
+		System.out.println(answer);
+	}
+	
+	
+	public static void writeAnswer(String path, String line){
+		BufferedReader br = null;
+		File file = new File(path);
+		// if file doesnt exists, then create it
+		
+		try {
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		FileWriter fw = new FileWriter(file.getAbsoluteFile(),true);
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(line+"\n");	
+		bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	
+	 public static void main(String[] args){
+		 String file = args[0];
+		 File f = new File(file);
+		 WGraph g = new WGraph(file);
+		 fordfulkerson(g.getSource(),g.getDestination(),g,f.getAbsolutePath().replace(".txt",""));
+	 }
+}
